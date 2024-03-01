7881b2df7a0e0b2ec4d72bf34c34fbdcf74bb9fe
TommyHong <ToomyHong1913@outlook.com>
2024-02-29 18:04:32 -0800

    add git

---
+++ src/abi/CheezeWizardsBasicTournament.ts
@@ -0,0 +1,5 @@
+import { PartialReadonlyContractAbi } from "../types";
+import CheezeWizardsBasicTournamentAbi from "./CheezeWizardsBasicTournamentAbi.json";
+
+export const CheezeWizardsBasicTournament =
+  CheezeWizardsBasicTournamentAbi as PartialReadonlyContractAbi;
---
+++ src/abi/DecentralandEstates.ts
@@ -0,0 +1,5 @@
+import { PartialReadonlyContractAbi } from "../types";
+import DecentralandEstatesAbi from "./DecentralandEstatesAbi.json";
+
+export const DecentralandEstates =
+  DecentralandEstatesAbi as PartialReadonlyContractAbi;
